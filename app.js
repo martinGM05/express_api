57ced503cd1a74cef9ad22eeb01dde63909ab5db
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-21 23:21:14 -0500

    Created Endpoint Post

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.get('/v1/explorers/:id', (req, res) => {
     res.status(200).json(explorer)
 })
 
+app.post('/v1/explorers', (req, res) => {
+    console.log(`Api Eplorers POST request ${new Date()}`)
+    const requestBody = req.body
+    res.status(201).json({
+        msg: 'Created'
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listenng on port ${port}`)
