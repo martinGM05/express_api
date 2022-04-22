160d035f659a443bb4529d76b4ac14bcb2dad401
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-21 23:12:07 -0500

    Create an Endpoint where returns a Explorer using an ID

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.get('/v1/explorers', (req, res) => {
     res.status(200).json(explorers)
 })
 
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id: ${req.params.id}`)
+    const explorer = {id: 1, name: "Martin"}
+    res.status(200).json(explorer)
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listenng on port ${port}`)
